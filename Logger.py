c732aa69f1ba893c1918f9df339b2b1722047c8a
Richard Bowmaker <richard.personal@outlook.com>
2022-02-23 16:49:50 +0000

    layed out the gui with tab control and trees

---
+++ Logger.py
@@ -0,0 +1,15 @@
+
+import tkinter as tk
+from tkinter import ttk
+
+def set_logger_listbox(lb: tk.Listbox) -> None:
+    global list_box
+    list_box = lb
+
+def log_info(text: str) ->None:
+    list_box.insert(tk.END, text)
+
+def log_error(text: str) -> None:
+    list_box.insert(tk.END, text)
+    n = list_box.size()
+    list_box.itemconfig(n - 1, {'fg': 'red'})
--- Main.py
+++ Main.py
@@ -1,175 +1,133 @@
 
-# from tkinter import *
-# from tkinter.ttk import *
-
 import tkinter as tk
 from tkinter import ttk
 from StatementEntry import *
+from Logger import *
 
-# class Example(Frame):
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.initUI()
-#
-#
-#     def initUI(self):
-#
-#         self.master.title("Review")
-#         self.pack(fill=BOTH, expand=True)
-#
-#         frame1 = Frame(self)
-#         frame1.pack(fill=X)
-#
-#         lbl1 = Label(frame1, text="Title", width=6)
-#         lbl1.pack(side=LEFT, padx=5, pady=5)
-#
-#         entry1 = Entry(frame1)
-#         entry1.pack(fill=X, padx=5, expand=True)
-#
-#         frame2 = Frame(self)
-#         frame2.pack(fill=X)
-#
-#         lbl2 = Label(frame2, text="Author", width=6)
-#         lbl2.pack(side=LEFT, padx=5, pady=5)
-#
-#         entry2 = Entry(frame2)
-#         entry2.pack(fill=X, padx=5, expand=True)
-#
-#         frame3 = Frame(self)
-#         frame3.pack(fill=BOTH, expand=True)
-#
-#         # lbl3 = Label(frame3, text="Review", width=6)
-#         # lbl3.pack(side=LEFT, anchor=N, padx=5, pady=5)
-#         #
-#         # txt = Text(frame3)
-#         # txt.pack(fill=BOTH, pady=5, padx=5, expand=True)
-#
-#         #----------------------------------------------
-#         # treeview https://www.pythontutorial.net/tkinter/tkinter-treeview/
-#
-#         columns = ('first_name', 'last_name', 'email')
-#         tree = Treeview(frame3, columns=columns, show='headings')
-#         tree.pack(fill=BOTH, expand=True)
-#
-#         tree.heading('first_name', text='First Name')
-#         tree.heading('last_name', text='Last Name')
-#         tree.heading('email', text='Email')
-#
-#         # add some data
-#         contacts = []
-#         for n in range(1, 100):
-#             contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
-#         for contact in contacts:
-#             tree.insert('', END, values=contact)
-#
-#         # add a scrollbar
-#         scrollbar = Scrollbar(frame3, orient=VERTICAL)
-#         tree.configure(yscroll=scrollbar.set)
-#         scrollbar.pack(fill=BOTH, expand=True)
+# -------------------------------------------
+# logger functions
 
-class ScrollableFrame(ttk.Frame):
-    def __init__(self, container, *args, **kwargs):
-        super().__init__(container, *args, **kwargs)
-        canvas = tk.Canvas(self)
-        scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
-        self.scrollable_frame = ttk.Frame(canvas)
+# def LogInfo(text: str) ->None:
+#     logger.insert("end", text)
 
-        self.scrollable_frame.bind(
-            "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all")
-            )
-        )
 
-        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-
-        canvas.configure(yscrollcommand=scrollbar.set)
-
-        canvas.pack(side="left", fill="both", expand=True)
-        scrollbar.pack(side="right", fill="y")
 
 if __name__ == "__main__":
 
     # https://www.youtube.com/watch?v=-rVA37OVDs8
 
     window = tk.Tk()
-    window.geometry("600x600+300+300")
+    window.geometry("1000x600")
 
-    # container = tk.Frame(window, width=400, bg="green")
-    # canvas = tk.Canvas(container, width=200, height=600, bg="red")
-    #
-    # scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
-    # scrollable_frame = ttk.Frame(canvas, width=200, height=600)
-    #
-    # scrollable_frame.bind(
-    #     "<Configure>",
-    #     lambda e: canvas.configure(
-    #         scrollregion=canvas.bbox("all")
-    #     )
-    # )
-    #
-    # canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
-    # canvas.configure(yscrollcommand=scrollbar.set)
-    #
-    # # for i in range(50):
-    # #     ttk.Label(scrollable_frame, text="Sample scrolling label", width=200).pack()
-    #
-    # columns = ('first_name', 'last_name', 'email')
-    # tree = ttk.Treeview(scrollable_frame, columns=columns, show='headings', height=600)
-    #
-    # tree.heading('first_name', text='First Name')
-    # tree.heading('last_name', text='Last Name')
-    # tree.heading('email', text='Email')
-    #
-    #
-    # container.pack(fill=tk.BOTH, expand=True)
-    # canvas.pack(side="left", fill=tk.BOTH, expand=True)
-    # scrollbar.pack(side="right", fill=tk.Y)
-    # tree.pack(fill=tk.BOTH, expand=True)
-    #
-    # # add some data
-    # contacts = []
-    # for n in range(1, 100):
-    #     contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
-    # for contact in contacts:
-    #     tree.insert('', tk.END, values=contact)
+    # ----------------------------------------------------
+    # frame1 has 3 data views in a tabbed control
+    # tab1 = list of all statement entries
+    # tab2 = tree view, weekly summary
+    # tab3 = tree view, monthly summary
 
-
-    # frame
-    frame1 = tk.Frame(master=window)
+    frame1 = tk.Frame(master=window, width=300)
     frame1.pack(pady=20, fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-    # scroll bar
-    tree_scroll = tk.Scrollbar(frame1)
-    tree_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+    # tab control
+    tabs = ttk.Notebook(frame1, width=400)
+    tab_all = ttk.Frame(tabs)
+    tab_weekly = ttk.Frame(tabs)
+    tab_monthly = ttk.Frame(tabs)
+    tabs.add(tab_all, text='All')
+    tabs.add(tab_weekly, text='Weekly')
+    tabs.add(tab_monthly, text='Monthly')
+    tabs.pack(fill=tk.BOTH, expand=True)
 
-    # tree view
+    # --------------------------------------------------
+    # weekly summary tree view
+    # weekly tree scroll bar
+    tree_weekly_scroll = tk.Scrollbar(tab_weekly)
+    tree_weekly_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # weekly tree view
     columns = ('first_name', 'last_name', 'email')
-    tree = ttk.Treeview(frame1, columns=columns, show='headings', yscrollcommand=tree_scroll.set)
-    tree.pack(fill=tk.BOTH, expand=True)
+    tree_weekly = ttk.Treeview(tab_weekly, columns=columns, show='headings', yscrollcommand=tree_weekly_scroll.set)
+    tree_weekly.pack(fill=tk.BOTH, expand=True)
+    tree_weekly.column(0, minwidth=100, width=100, stretch=False)
+    tree_weekly.column(1, minwidth=200, width=200, stretch=False)
+    tree_weekly.column(2, minwidth=200, width=200, stretch=False)
+    tree_weekly.heading('first_name', text='First Name')
+    tree_weekly.heading('last_name', text='Last Name')
+    tree_weekly.heading('email', text='Email')
 
     # configure scroll bar
-    tree_scroll.config(command=tree.yview)
-
-    tree.heading('first_name', text='First Name')
-    tree.heading('last_name', text='Last Name')
-    tree.heading('email', text='Email')
+    tree_weekly_scroll.config(command=tree_weekly.yview)
 
     # add some data
     contacts = []
     for n in range(1, 100):
-        contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+        contacts.append((f'weekly {n}', f'last {n}', f'email{n}@example.com'))
     for contact in contacts:
-        tree.insert('', tk.END, values=contact)
+        tree_weekly.insert('', tk.END, values=contact)
 
-    frame2 = tk.Frame(master=window, width=400, bg="yellow")
+    # --------------------------------------------------
+    # monthly summary tree view
+    # monthly tree scroll bar
+    tree_monthly_scroll = tk.Scrollbar(tab_monthly)
+    tree_monthly_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # weekly tree view
+    columns = ('first_name', 'last_name', 'email')
+    tree_monthly = ttk.Treeview(tab_monthly, columns=columns, show='headings', yscrollcommand=tree_monthly_scroll.set)
+    tree_monthly.pack(fill=tk.BOTH, expand=True)
+    tree_monthly.column(0, minwidth=100, width=100, stretch=False)
+    tree_monthly.column(1, minwidth=200, width=200, stretch=False)
+    tree_monthly.column(2, minwidth=200, width=200, stretch=False)
+    tree_monthly.heading('first_name', text='First Name')
+    tree_monthly.heading('last_name', text='Last Name')
+    tree_monthly.heading('email', text='Email')
+
+    # configure scroll bar
+    tree_monthly_scroll.config(command=tree_monthly.yview)
+
+    # add some data
+    contacts = []
+    for n in range(1, 100):
+        contacts.append((f'monthly {n}', f'last {n}', f'email{n}@example.com'))
+    for contact in contacts:
+        tree_monthly.insert('', tk.END, values=contact)
+
+    # ---------------------------------------------------
+    # frame2
+    frame2 = tk.Frame(master=window, width=400)
     frame2.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
+    # ---------------------------------------------------
+    # frame3 has the logger
     frame3 = tk.Frame(master=window, width=400, bg="blue")
     frame3.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
+    # logger scroll bar
+    logger_scroll = tk.Scrollbar(frame3)
+    logger_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+    logger = tk.Listbox(frame3, yscrollcommand=logger_scroll.set)
+    logger.pack(fill=tk.BOTH, expand=True)
+
+    # configure scroll bar
+    logger_scroll.config(command=logger.yview)
+
+    # initialise logger
+    set_logger_listbox(logger)
+    log_info('info')
+    log_error('error')
+    log_info('info')
+    log_error('error')
+    log_info('info')
+    log_error('error')
+    log_info('info')
+    log_error('error')
+    se = StatementEntry()
+    log_info(se)
+
     window.mainloop()
+
+
 
 
 
